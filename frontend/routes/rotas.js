608d8fb6bef635e73453e5c2ee27f7ee974d21b5
MarcoAurelioArJes <marcoaureliodea.jesus@gmail.com>
2023-05-22 14:31:45 -0300

    Falta estilizar tela de detalhes

--- frontend/routes/rotas.js
+++ frontend/routes/rotas.js
@@ -1,8 +1,18 @@
-const ejs = require('ejs');
+const tecnologias = require('../tecnologias/listaTecnologias');
 
 const rotas = (app) => {
     app.get("/", (req, res) => {
-        res.render('index');
+        res.render('index', {
+            informacoes: tecnologias
+        });
+    });
+
+    app.get("/detalhes/:id", (req, res) => {
+        let index = tecnologias.findIndex(t => t.id == req.params.id)
+
+        res.render('../views/components/detalhes', {
+            informacao: tecnologias[index]
+        });
     });
 }
 
---
+++ frontend/tecnologias/listaTecnologias.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        nome: "IA (Inteligência Artifical)",
+        imagem: "/images/imagem_IA.png",
+        descricao: "A IA está transformando inúmeras indústrias, desde assistentes virtuais em nossos dispositivos até sistemas de diagnóstico médico avançados. Explore as últimas tendências em IA, como aprendizado de máquina, processamento de linguagem natural e visão computacional.",
+        softwares: [
+            {
+                nome: "chatGPT",
+                descricao: "chat de perguntas e respostas"
+            }
+        ]
+    }
+]
