63922a448e74ca829926ffe3587e2bc305f240b7
MarcoAurelioArJes <marcoaureliodea.jesus@gmail.com>
2023-05-29 22:31:16 -0300

    front

--- backend/routes/rotasObservatorio.js
+++
@@ -1,6 +0,0 @@
-const observatorioController = require("../controller/observatorioController");
-
-module.exports = (app) => {
-    app.post('/criar', observatorioController.criar);
-    app.get('/listarPorId/:id', observatorioController.listarPorId);
-}
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-require('./repositorio/database/connect');
-require('./repositorio/tabelas/criarTabelas');
-
-require('./routes/rotasObservatorio')(app);
-
-const PORT = 3000;
-app.listen(PORT, () => console.log('server rodando na porta ' + PORT));
